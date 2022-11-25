8f6d877f08b94605febb4150bb41b98c802ea856
Alawi ALqushaibi <alawi.alqushaibi@gmail.com>
2022-11-25 11:04:06 +0800

    week8 activity-1.2.r added

--- week8/Activity-1.2.r
+++ week8/Activity-1.2.r
@@ -1,6 +1,20 @@
-my_matrix <- matrix(c(1, 2, 3, 4, 5, 6), nrow=2)
+my_matrix <- matrix(c(1:30), nrow=10)
 print(my_matrix)
 df_from_matrix <- data.frame(my_matrix)
 print(df_from_matrix)
 names(df_from_matrix) <- c('col_1', 'col_2', 'col_3')
 print(df_from_matrix)
+df = df_from_matrix
+print(df)
+
+df1 = df[,-3]
+print(df1)
+
+df2 = df[-3,]
+print(df2)
+
+df3 = df[,3]
+print(df3)
+
+df4 = df[3,]
+print(df4)
